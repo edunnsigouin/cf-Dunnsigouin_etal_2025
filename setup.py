2b1e2c1c2f8a098aca84095b72c05f5db6e35b8d
Etienne Dunn-Sigouin <Etienne.Dunn-Sigouin@uib.no>
2022-08-30 09:43:00 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "forsikring",
+    version = "0.0.1",
+    author = "Etienne Dunn-Sigouin",
+    packages=['forsikring'],
+    install_requires=["numpy","xarray","scipy"],
+)
+
