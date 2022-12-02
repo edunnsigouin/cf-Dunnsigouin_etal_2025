e8d6bb7b00d50a0f5349c03a047f3a2c53e33e4c
Etienne Dunn-Sigouin <Etienne.Dunn-Sigouin@uib.no>
2022-12-02 11:50:04 +0100

    update

--- code/preprocess/calc-daily-from-6hourly-s2s-ecmwf.py
+++ code/preprocess/calc-daily-from-6hourly-s2s-ecmwf.py
@@ -8,11 +8,12 @@ import xarray   as xr
 import pandas   as pd
 import os
 from forsikring import date_to_model  as d2m
-from forsikring import config,misc,s2smisc
+from forsikring import config,misc,s2s
+from matplotlib  import pyplot as plt
 
 # INPUT ----------------------------------------------- 
-variables     = ['tp']                  
-dtypes        = ['cf']             # control & perturbed forecasts/hindcasts
+variables     = ['sf']                  
+dtypes        = ['cf']                  # control & perturbed forecasts/hindcasts
 product       = 'hindcast'              # hindcast or forecast ?
 mon_thu_start = ['20210104','20210107'] # first monday & thursday initialization date of forecast
 num_i_weeks   = 1                       # number of forecasts/hindcast intialization dates to download
@@ -21,15 +22,8 @@ comp_lev      = 5                       # level of compression with nccopy (1-10
 write2file    = False
 # -----------------------------------------------------            
 
-# define encoding for compression when writing to file
-#encoding = {'dtype':'int16',
-#            'scale_factor':9.26033143309001e-06,
-#            'add_offset':0.303424019736627,
-#            '_FillValue':-9999,
-#            'missing_value':-9999}
-
-
-dates_monday_thursday = s2smisc.get_monday_thursday_dates(mon_thu_start,num_i_weeks)
+# get all dates for monday and thursday forecast initializations
+dates_monday_thursday = s2s.get_monday_thursday_dates(mon_thu_start,num_i_weeks)
 
 for variable in variables:
     for date in dates_monday_thursday:
@@ -43,122 +37,78 @@ for variable in variables:
             path_in    = config.dirs[product + '_6hourly']
             path_out   = config.dirs[product + '_daily']
 
-            if grid == '0.25/0.25':
-                gridstring = '0.25x0.25'
-            elif grid == '0.5/0.5':
-                gridstring = '0.5x0.5'
+            if grid == '0.25/0.25': gridstring = '0.25x0.25'
+            elif grid == '0.5/0.5': gridstring = '0.5x0.5'
                 
             forcastcycle  = d2m.which_mv_for_init(datestring,model='ECMWF',fmt='%Y-%m-%d')
             basename      = '%s_%s_%s_%s'%(forcastcycle,gridstring,datestring,dtype)
                 
             if variable == 'tp': # daily accumulated precip (m)
-                filename_in               = path_in + variable + '/' + variable + '_' + basename + '.nc'
+                filename_in                      = path_in + variable + '/' + variable + '_' + basename + '.nc'
+                filename_out                     = path_out + variable + '/' + variable + '_' + basename + '.nc'
+                ds                               = xr.open_dataset(filename_in)
+                ds                               = ds.resample(time='1D').sum('time')
+                ds[variable].attrs['units']      = 'm'
+                ds[variable].attrs['long_name']  = 'daily accumulated precipitation'
+                if write2file: ds.to_netcdf(filename_out)  
+                ds.close()
+
+            elif variable == 'rn': # daily accumulated rain (precip - snowfall, m)  
+                filename1_in                     = path_in + 'tp/' + 'tp' + '_' + basename + '.nc'
+                filename2_in                     = path_in + 'sf/' + 'sf' + '_' + basename + '.nc'
+                filename_out                     = path_out + variable + '/' + variable + '_' + basename + '.nc' 
+                ds1                              = xr.open_dataset(filename1_in)
+                ds2                              = xr.open_dataset(filename2_in)
+                ds1                              = ds1.resample(time='1D').sum('time')
+                ds2                              = ds2.resample(time='1D').sum('time')
+                ds1['tp']                        = ds1['tp'] - ds2['sf']
+                ds1                              = ds1.rename({'tp':variable})
+                ds1[variable].attrs['units']     = 'm'
+                ds1[variable].attrs['long_name'] = 'daily accumulated rainfall'
+                if write2file: ds1.to_netcdf(filename_out)
+                ds1.close()
+                ds2.close()
+
+            elif variable == 'mxtp6': # daily maximum 6 hour accumulated precip (m)
+                filename_in                      = path_in + 'tp/' + 'tp' + '_' + basename + '.nc'
+                filename_out                     = path_out + variable + '/' + variable + '_' + basename + '.nc'
+                ds                               = xr.open_dataset(filename_in)
+                ds                               = ds.resample(time='1D').max('time')
+                ds                               = ds.rename({'tp':variable})
+                ds[variable].attrs['units']      = 'm'
+                ds[variable].attrs['long_name']  = 'daily maximum 6 hour accumulated precipitation'
+                if write2file: ds.to_netcdf(filename_out)
+                ds.close()
+
+            elif variable == 'mxtpr': # daily maximum timestep precipitation rate (kgm-2s-1)
+                filename_in                     = path_in + variable + '/' + variable + '_' + basename + '.nc'
+                filename_out                    = path_out + variable + '/' + variable + '_' + basename + '.nc'
+                ds                              = xr.open_dataset(filename_in)
+                ds                              = ds.resample(time='1D').max('time')
+                ds[variable].attrs['units']     = 'kg m**-2 s**-1'
+                ds[variable].attrs['long_name'] = 'daily maximum timestep precipitation rate'
+                if write2file: ds.to_netcdf(filename_out)
+                ds.close()
+            
+            elif variable == 'mxrn6': # daily maximum 6 hour accumulated rain (precip - snowfall, m)
+                filename1_in              = path_in + 'tp/' + 'tp' + '_' + basename + '.nc'
+                filename2_in              = path_in + 'sf/' + 'sf' + '_' + basename + '.nc'
                 filename_out              = path_out + variable + '/' + variable + '_' + basename + '.nc'
+                ds1                       = xr.open_dataset(filename1_in)
+                ds2                       = xr.open_dataset(filename2_in)
+                da                        = (ds1['tp'] - ds2['sf']).resample(time='1D').max('time')
+                da.name                   = variable
+                da.attrs['units']         = 'm'
+                da.attrs['long_name']     = 'daily maximum 6 hour accumulated rainfall'
+                if write2file: da.to_netcdf(filename_out)
+                ds1.close()
+                ds2.close()
+                da.close()
 
-                """
-                ds                        = xr.open_dataset(filename_in,mask_and_scale=False)
-                scale_factor              = ds[variable].attrs['scale_factor']
-                add_offset                = ds[variable].attrs['add_offset']
-                _FillValue                = ds[variable].attrs['_FillValue']
-                missing_value             = ds[variable].attrs['missing_value']
-                encoding                  = {variable:{'dtype':'int16','scale_factor':scale_factor,'add_offset':add_offset,
-                                                       '_FillValue':_FillValue,'missing_value':missing_value}}
-                ds.close()
-                """
+            print('compress file to reduce space..')
+            s2s.compress_file(comp_lev,3,filename_out,path_out + variable + '/')
 
-                ds                        = xr.open_dataset(filename_in)
-                #ds                        = ds.resample(time='1D').sum('time')
-                #ds.tp.attrs['units']      = 'm'
-                #ds.tp.attrs['long_name']  = 'daily accumulated precipitation'
+            misc.toc()
 
-                dataMin      = ds[variable].min().values
-                dataMax      = ds[variable].max().values
-                add_offset   = dataMin
-                scale_factor = (dataMax - dataMin) / (2^1000 - 1)
-                print(scale_factor)
-                encoding     = {variable:{'dtype':'int16',
-                                          'scale_factor':scale_factor,
-                                          'add_offset':add_offset,
-                                          '_FillValue':-32767,
-                                          'missing_value':-32767}}
 
-                if write2file:
-                    ds.to_netcdf(filename_out,encoding=encoding)
-                    #ds.to_netcdf(filename_out,encoding={variable:encoding})
-                    #ds.to_netcdf(filename_out)  
-                ds.close()
-                
-"""
-        elif variable == 'rn': # daily accumulated rain (precip - snowfall, m)
-            dir_in1      = path_in + 'tp/'
-            dir_in2      = path_in + 'sf/'
-            dir_out      = path_out + variable + '/'
-            filename1_in = 'tp_' + gridstring + '_' + str(year) + '.nc'
-            filename2_in = 'sf_' + gridstring + '_' + str(year) + '.nc'
-            ds1          = xr.open_dataset(dir_in1 + filename1_in)
-            ds2          = xr.open_dataset(dir_in2 + filename2_in)
-            ds1          = ds1.resample(time='1D').sum('time')
-            ds2          = ds2.resample(time='1D').sum('time')
-            ds1['tp']    = ds1['tp'] - ds2['sf']
-            if write2file:
-                filename_out              = 'rn_' + gridstring + '_' + str(year) + '.nc'
-                ds1                       = ds1.rename({'tp':'rn'})
-                ds1.rn.attrs['units']     = 'm'
-                ds1.rn.attrs['long_name'] = 'daily accumulated rainfall'
-                ds1.to_netcdf(dir_out + filename_out)
-            ds1.close()
-            ds2.close()
-            
-        elif variable == 'mxtp6': # daily maximum 6 hour accumulated precip (m)
-            dir_in                    = path_in + 'tp/'
-            dir_out                   = path_out + variable + '/'
-            filename_in               = 'tp_' + gridstring + '_' + str(year) + '.nc'
-            filename_out              =	variable + '_' + gridstring + '_' + str(year) + '.nc'
-            ds                        = xr.open_dataset(dir_in + filename_in)
-            ds                        = ds.resample(time='1D').max('time')
-            ds.tp.attrs['units']      = 'm'
-            ds.tp.attrs['long_name']  = 'daily maximum 6 hour accumulated precipitation'
-            if write2file:
-                ds.to_netcdf(dir_out + filename_out)
-            ds.close()
 
-        elif variable == 'mxtpr': # daily maximum timestep precipitation rate (kgm-2s-1)
-            dir_in                      = path_in + variable + '/'
-            dir_out                     = path_out + variable + '/'
-            filename_in                 = variable + '_' + gridstring + '_' + str(year) + '.nc'
-            filename_out                = variable + '_' + gridstring + '_' + str(year) + '.nc'
-            ds                          = xr.open_dataset(dir_in + filename_in)
-            ds                          = ds.resample(time='1D').max('time')
-            ds.mxtpr.attrs['units']     = 'kg m**-2 s**-1'
-            ds.mxtpr.attrs['long_name'] = 'daily maximum timestep precipitation rate'
-            if write2file:
-                ds.to_netcdf(dir_out + filename_out)
-            ds.close()
-            
-        elif variable == 'mxrn6': # daily maximum 6 hour accumulated rain (precip - snowfall, m)
-            dir_in1      = path_in + 'tp/'
-            dir_in2      = path_in + 'sf/'
-            dir_out      = path_out + variable + '/'
-            filename1_in = 'tp_' + gridstring + '_' + str(year) + '.nc'
-            filename2_in = 'sf_' + gridstring + '_' + str(year) + '.nc'
-            ds1          = xr.open_dataset(dir_in1 + filename1_in)
-            ds2          = xr.open_dataset(dir_in2 + filename2_in)
-            da           = (ds1['tp'] - ds2['sf']).resample(time='1D').max('time')
-            if write2file:
-                filename_out             = variable + '_' + gridstring + '_' + str(year) + '.nc'
-                da.name                  = variable
-                da.attrs['units']        = 'm'
-                da.attrs['long_name']    = 'daily maximum 6 hour accumulated rainfall'
-                da.to_netcdf(dir_out + filename_out)
-            ds1.close()
-            ds2.close()
-            da.close()
-            
-        print('compress file to reduce space..')
-        cmd               = 'nccopy -k 3 -s -d ' + str(comp_lev) + ' '
-        filename_out_comp = 'compressed_' + variable + '_' + gridstring + '_' + str(year) + '.nc'
-        os.system(cmd + dir_out + filename_out + ' ' + dir_out + filename_out_comp)
-        os.system('mv ' + dir_out + filename_out_comp + ' ' + dir_out + filename_out)
-        
-        misc.toc()
-"""
--- forsikring/s2s.py
+++ forsikring/s2s.py
@@ -4,15 +4,25 @@ Collection of miscellaneous functions for ecmwf s2s model data
 import numpy    as np
 import xarray   as xr
 import pandas   as pd
+import os
 
 
 def get_monday_thursday_dates(mon_thu_start,num_i_weeks):
     """
     generate set of continuous monday and thursday dates starting on mon_thu_start[0] and mon_thu_start[1] respectively
     """
-    
     dates_monday          = pd.date_range(mon_thu_start[0], periods=num_i_weeks, freq="7D") # forecasts that start monday
     dates_thursday        = pd.date_range(mon_thu_start[1], periods=num_i_weeks, freq="7D") # forecasts that start thursday
     dates_monday_thursday = dates_monday.union(dates_thursday)
-
     return dates_monday_thursday
+
+
+def compress_file(comp_lev,ncfiletype,filename,path_out):
+    """
+    wrapper for compressing file using nccopy
+    """
+    cmd           = 'nccopy -k ' + str(ncfiletype) + ' -s -d ' + str(comp_lev) + ' '
+    filename_comp = path_out + 'temp.nc'
+    os.system(cmd + filename + ' ' + filename_comp)
+    os.system('mv ' + filename_comp + ' ' + filename)
+    return
