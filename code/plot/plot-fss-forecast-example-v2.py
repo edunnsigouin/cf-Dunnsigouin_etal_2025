6988720bc541e8d86c8d2ad3c4a3dae85e8cba11
Etienne Dunn-Sigouin <Etienne.Dunn-Sigouin@uib.no>
2023-03-28 09:47:35 +0200

    update

--- code/plot/plot-fss-forecast-example-v2.py
+++ code/plot/plot-fss-forecast-example-v2.py
@@ -10,7 +10,7 @@ from forsikring  import misc,s2s,config
 
 # INPUT -----------------------
 RF_flag           = 'clim'                   # clim or pers 
-time_flag         = 'time'                   # time or timescale
+time_flag         = 'timescale'                   # time or timescale
 variable          = 'tp24'                   # tp24,rn24,mx24rn6,mx24tp6,mx24tpr
 domain            = 'europe'                 # europe/nordic/vestland                       
 init_start        = '20210104'               # first initialization date of forecast (either a monday or thursday)
@@ -46,11 +46,17 @@ fontsize = 11
 figsize  = np.array([4*1.61,4])
 fig,ax   = plt.subplots(nrows=1,ncols=1,figsize=(figsize[0],figsize[1]))
 
-ax.errorbar(x-1.5,y[0,:],yerr=[yerr1[0,:],yerr2[0,:]],fmt='o',c='k',ecolor='k',elinewidth=2,label='1 day',linestyle='-')
-ax.errorbar(x-0.75,y[4,:],yerr=[yerr1[4,:],yerr2[4,:]],fmt='o',c='tab:blue',ecolor='tab:blue',elinewidth=2,label='5 day',linestyle='-')
-ax.errorbar(x,y[7,:],yerr=[yerr1[7,:],yerr2[7,:]],fmt='o',c='tab:green',ecolor='tab:green',elinewidth=2,label='8 day',linestyle='-')
-ax.errorbar(x+0.75,y[9,:],yerr=[yerr1[9,:],yerr2[9,:]],fmt='o',c='tab:red',ecolor='tab:red',elinewidth=2,label='10 day',linestyle='-')
-
+if time_flag == 'time':
+    ax.errorbar(x-1.5,y[0,:],yerr=[yerr1[0,:],yerr2[0,:]],fmt='o',c='k',ecolor='k',elinewidth=2,label='1 day',linestyle='-')
+    ax.errorbar(x-0.75,y[4,:],yerr=[yerr1[4,:],yerr2[4,:]],fmt='o',c='tab:blue',ecolor='tab:blue',elinewidth=2,label='5 day',linestyle='-')
+    ax.errorbar(x,y[7,:],yerr=[yerr1[7,:],yerr2[7,:]],fmt='o',c='tab:green',ecolor='tab:green',elinewidth=2,label='8 day',linestyle='-')
+    ax.errorbar(x+0.75,y[9,:],yerr=[yerr1[9,:],yerr2[9,:]],fmt='o',c='tab:red',ecolor='tab:red',elinewidth=2,label='10 day',linestyle='-')
+elif time_flag == 'timescale':
+    ax.errorbar(x-1.5,y[0,:],yerr=[yerr1[0,:],yerr2[0,:]],fmt='o',c='k',ecolor='k',elinewidth=2,label='1d1d',linestyle='-')
+    ax.errorbar(x-0.75,y[1,:],yerr=[yerr1[1,:],yerr2[1,:]],fmt='o',c='tab:blue',ecolor='tab:blue',elinewidth=2,label='2d2d',linestyle='-')
+    ax.errorbar(x,y[2,:],yerr=[yerr1[2,:],yerr2[2,:]],fmt='o',c='tab:green',ecolor='tab:green',elinewidth=2,label='4d4d',linestyle='-')
+    ax.errorbar(x+0.75,y[3,:],yerr=[yerr1[3,:],yerr2[3,:]],fmt='o',c='tab:red',ecolor='tab:red',elinewidth=2,label='1w1w',linestyle='-')
+    
 ax.legend(frameon=False,ncol=4,fontsize=fontsize,loc='lower center')
 ax.axhline(y=0.0, color='k', linestyle='-',linewidth=0.75)
 ax.set_xticks(x)
