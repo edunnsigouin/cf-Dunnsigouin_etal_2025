822c05f6af3d91fa0c88b71bfbc4e128507c3461
Etienne Dunn-Sigouin <Etienne.Dunn-Sigouin@uib.no>
2023-11-28 15:38:55 +0100

    update

--- code/plot/plot-fig-02.py
+++ code/plot/plot-fig-02.py
@@ -17,20 +17,27 @@ def setup_subplot(ax, time, box_size, fss_data, sig_data, title_text, clevs, cma
 
     contour = ax.contour(time, box_size, fss_data, levels=clevs, linewidths=1,linestyles='-',colors='grey')
     ax.clabel(contour, clevs, inline=True, fmt='%1.1f', fontsize=fontsize)
-    
+
     ax.set_xticks(time)
-    ax.set_xticklabels(['1', '', '3', '', '5', '', '7', '', '9', '', '11', '', '13', '', '15'], fontsize=fontsize)
-    if (title_text == 'c) summer temperature') or (title_text == 'd) winter temperature'):
-        ax.set_xlabel(r'lead time [days]', fontsize=fontsize)
     ax.set_xlim([time[0], time[-1]])
+    if time.size == 6:
+        ax.set_xticklabels(['1','2','3','4','5','6'],fontsize=fontsize)
+        ax.set_xlabel(r'lead time [weeks]',fontsize=fontsize)
+    else:
+        ax.set_xticklabels(['1', '', '3', '', '5', '', '7', '', '9', '', '11', '', '13', '', '15'], fontsize=fontsize)
+        ax.set_xlabel(r'lead time [days]', fontsize=fontsize)
 
     ax.set_yticks(np.array([1, 9, 17, 25, 33, 41, 49, 57]))
-    ax.set_ylim([box_size[0], box_size[-2]])
-    ax.set_title(title_text, fontsize=fontsize + 3)
     ax.set_yticklabels(['1/9', '9/81', '17/153', '25/225', '33/297', '41/369', '49/441', '57/513'], fontsize=fontsize)
-    if (title_text == 'a) summer precipitation') or (title_text == 'c) summer temperature'):
+    if (title_text == 'a) daily-mean annual precipitation') or (title_text == 'c) daily-mean summer precipitation'):
         ax.set_ylabel(r'spatial scale [gridpoints$^2$/km$^2$]', fontsize=fontsize)
+    ax.set_ylim([box_size[0], box_size[-2]])
 
+    ax.set_title(title_text, fontsize=fontsize + 3)
+
+    # horizontal line to show edge effects of domain
+    ax.axhline(y=41,xmin=0,xmax=time.size,color='r',linewidth=2)
+    
     """
     cb = plt.colorbar(p, ax=ax, orientation='vertical', ticks=clevs, pad=0.025, aspect=15)
     cb.ax.set_title('fss', fontsize=fontsize)
@@ -45,17 +52,21 @@ write2file = True
 # define stuff         
 path_in           = config.dirs['verify_s2s_forecast_daily']
 path_out          = config.dirs['fig'] + 'paper/'
+filename_in_1     = 'fss_tp24_daily_europe3_annual_2020-01-02_2022-12-29_0.25x0.25.nc'
+filename_in_2     = 'fss_tp24_weekly_europe3_annual_2021-01-04_2021-12-30.nc'
+filename_in_3     = 'fss_tp24_daily_europe3_mjjas_2020-05-04_2022-09-29_0.25x0.25.nc'
+filename_in_4     = 'fss_tp24_daily_europe3_ndjfm_2020-01-02_2022-12-29_0.25x0.25.nc'
 figname_out       = 'fig_02.pdf'
-filename_in_1     = 'fss_tp24_time_europe_mjjas_2020-05-04_2022-09-29_0.25x0.25.nc'
-filename_in_2     = 'fss_tp24_time_europe_ndjfm_2020-01-02_2022-12-29_0.25x0.25.nc'
-filename_in_3     = 'fss_t2m24_time_europe_mjjas_2021-05-03_2021-09-30_0.25x0.25.nc'
-filename_in_4     = 'fss_t2m24_time_europe_ndjfm_2021-01-04_2021-12-30_0.25x0.25.nc'
 
 # read in data
 ds1        = xr.open_dataset(path_in + filename_in_1)
-ds2        = xr.open_dataset(path_in + filename_in_2)
+ds2        = xr.open_dataset(path_in + filename_in_2)    
 ds3        = xr.open_dataset(path_in + filename_in_3)
 ds4        = xr.open_dataset(path_in + filename_in_4)
+
+# Remove box sizes where low and high-res data don't overlap on the same grid in timescale dimension data
+index         = np.where(~np.isnan(ds2['fss'][:,4]))[0]
+ds2           = ds2.isel(box_size=index)
 
 # calculate significance
 sig1       = s2s.mask_significant_values_from_bootstrap(ds1['fss_bootstrap'],0.05)
@@ -66,23 +77,20 @@ sig4       = s2s.mask_significant_values_from_bootstrap(ds4['fss_bootstrap'],0.0
 # plot 
 fontsize   = 11
 clevs      = np.arange(0.0, 1.1, 0.1)
-cmap1      = mpl.cm.get_cmap("GnBu").copy()
-cmap2      = mpl.cm.get_cmap("YlOrBr").copy()
+cmap       = mpl.cm.get_cmap("GnBu").copy()
 figsize    = np.array([12,8])
-fig,ax     = plt.subplots(nrows=2,ncols=2,sharey='row',sharex='col',figsize=(figsize[0],figsize[1]))
+fig,ax     = plt.subplots(nrows=2,ncols=2,sharey='row',figsize=(figsize[0],figsize[1]))
 ax         = ax.ravel()
 
-# A) MJJAS precipitation
-setup_subplot(ax[0], ds1['time'], ds1['box_size'], ds1['fss'], sig1, 'a) summer precipitation', clevs, cmap1, fontsize)
+# A) daily precipitation
+setup_subplot(ax[0], ds1['time'], ds1['box_size'], ds1['fss'], sig1, 'a) daily-mean annual precipitation', clevs, cmap, fontsize)
 
-# B) NDJFM precipitation
-setup_subplot(ax[1], ds2['time'], ds2['box_size'], ds2['fss'], sig2, 'b) winter precipitation', clevs, cmap1, fontsize)
+# B) weekly precipitation 
+setup_subplot(ax[1], ds2['time'], ds2['box_size'], ds2['fss'], sig2, 'b) weekly-mean annual precipitation', clevs, cmap, fontsize)
 
-# D) MJJAS temperature
-setup_subplot(ax[2], ds3['time'], ds3['box_size'], ds3['fss'], sig3, 'c) summer temperature', clevs, cmap2, fontsize)
+setup_subplot(ax[2], ds3['time'], ds3['box_size'], ds3['fss'], sig3, 'c) daily-mean summer precipitation', clevs, cmap, fontsize)
 
-# E) NDJFM temperature
-setup_subplot(ax[3], ds4['time'], ds4['box_size'], ds4['fss'], sig4, 'd) winter temperature', clevs, cmap2, fontsize)
+setup_subplot(ax[3], ds4['time'], ds4['box_size'], ds4['fss'], sig4, 'd) daily-mean winter precipitation', clevs, cmap, fontsize)
 
 # write2file
 plt.tight_layout()
--- code/plot/plot-fig-04.py
+++ code/plot/plot-fig-04.py
@@ -10,45 +10,39 @@ import matplotlib as mpl
 
 def setup_subplot(ax, time, box_size, fss_data, sig_data, title_text, clevs, cmap, fontsize):
     """
-    Sets up specifics of subplots for fig. 1
+    Sets up specifics of subplots for fig. 4
     """
-    if (title_text == 'c) >90$^{th}$ minus <10$^{th}$ quantile precipitation') or (title_text == 'f) >90$^{th}$ minus <10$^{th}$ quantile temperature'):
-        p = ax.contourf(time, box_size, fss_data, levels=clevs, cmap=cmap, extend='both')
-    else:
-        p = ax.contourf(time, box_size, fss_data, levels=clevs, cmap=cmap, extend='min')
+    p = ax.contourf(time, box_size, fss_data, levels=clevs, cmap=cmap, extend='min')
     ax.pcolor(time, box_size, sig_data, hatch='\\\\', cmap=mpl.colors.ListedColormap(['none']), edgecolor=[0.8,0.8,0.8], lw=0)
 
     contour = ax.contour(time, box_size, fss_data, levels=clevs, linewidths=1,linestyles='-',colors='grey')
-    ax.clabel(contour, clevs, inline=True, fmt='%1.2f', fontsize=fontsize)
+    ax.clabel(contour, clevs, inline=True, fmt='%1.1f', fontsize=fontsize)
 
     ax.set_xticks(time)
+    ax.set_xlim([time[0], time[-1]])
     if time.size == 6:
-        ax.set_xticklabels(['1d1d','2d2d','4d4d','1w1w','2w2w','4w3w'],fontsize=fontsize)
-        ax.set_xlabel(r'lead time [timescale]',fontsize=fontsize)
+        ax.set_xticklabels(['1','2','3','4','5','6'],fontsize=fontsize)
+        if title_text == 'd) <10$^{th}$ quantile weekly-mean precipitation':
+            ax.set_xlabel(r'lead time [weeks]',fontsize=fontsize)
     else:
         ax.set_xticklabels(['1', '', '3', '', '5', '', '7', '', '9', '', '11', '', '13', '', '15'], fontsize=fontsize)
-
-    if (title_text == 'd) >90$^{th}$ quantile temperature') or (title_text == 'e) <10$^{th}$ quantile temperature'): 
-        ax.set_xlabel(r'lead time [days]', fontsize=fontsize)
-    
-    ax.set_xlim([time[0], time[-1]])
+        if title_text == 'c) <10$^{th}$ quantile daily-mean precipitation':
+            ax.set_xlabel(r'lead time [days]', fontsize=fontsize)
 
     ax.set_yticks(np.array([1, 9, 17, 25, 33, 41, 49, 57]))
     ax.set_yticklabels(['1/9', '9/81', '17/153', '25/225', '33/297', '41/369', '49/441', '57/513'], fontsize=fontsize)
-    if (title_text == 'a) >90$^{th}$ quantile precipitation') or (title_text == 'd) >90$^{th}$ quantile temperature'):
+    if (title_text == 'a) >90$^{th}$ quantile daily-mean precipitation') or (title_text == 'c) <10$^{th}$ quantile daily-mean precipitation'):
         ax.set_ylabel(r'spatial scale [gridpoints$^2$/km$^2$]', fontsize=fontsize)
     ax.set_ylim([box_size[0], box_size[-2]])
 
     ax.set_title(title_text, fontsize=fontsize + 3)
     """
-    if (title_text == 'c) >90$^{th}$ minus <10$^{th}$ quantile precipitation') or (title_text == 'f) >90$^{th}$ minus <10$^{th}$ quantile temperature'):
-        cb = plt.colorbar(p, ax=ax, orientation='vertical', ticks=clevs[::2], pad=0.025, aspect=15)
-    else:
-        cb = plt.colorbar(p, ax=ax, orientation='vertical', ticks=clevs, pad=0.025, aspect=15)
-    cb.ax.set_title('fbss', fontsize=fontsize)
+    cb = plt.colorbar(p, ax=ax, orientation='vertical', ticks=clevs, pad=0.025, aspect=15)
+    cb.ax.set_title('fss', fontsize=fontsize)
     cb.ax.tick_params(labelsize=fontsize, size=0)
     """
     return ax
+
 
 # INPUT -----------------------
 write2file = True
@@ -57,60 +51,49 @@ write2file = True
 # define stuff         
 path_in           = config.dirs['verify_s2s_forecast_daily']
 path_out          = config.dirs['fig'] + 'paper/'
-filename_in_1     = 'fbss_tp24_pval0.9_time_europe_annual_2020-01-02_2022-12-29_0.25x0.25.nc'
-filename_in_2     = 'fbss_tp24_pval0.1_time_europe_annual_2020-01-02_2022-12-29_0.25x0.25.nc'
-filename_in_3     = 'fbss_difference_time_tp24_pval0.9_europe_annual_2020-01-02_2022-12-29_tp24_pval0.1_europe_annual_2020-01-02_2022-12-29_0.25x0.25.nc'
-filename_in_4     = 'fbss_t2m24_pval0.9_time_europe_annual_2021-01-04_2021-12-30_0.25x0.25.nc'
-filename_in_5     = 'fbss_t2m24_pval0.1_time_europe_annual_2021-01-04_2021-12-30_0.25x0.25.nc'
-filename_in_6     = 'fbss_difference_time_t2m24_pval0.9_europe_annual_2021-01-04_2021-12-30_t2m24_pval0.1_europe_annual_2021-01-04_2021-12-30_0.25x0.25.nc'
+filename_in_1     = 'fbss_tp24_pval0.9_daily_europe_annual_2020-01-02_2022-12-29_0.25x0.25.nc'
+filename_in_2     = 'fbss_tp24_pval0.9_weekly_europe_annual_2021-01-04_2021-12-30.nc'
+filename_in_3     = 'fbss_tp24_pval0.1_daily_europe_annual_2020-01-02_2022-12-29_0.25x0.25.nc'
+filename_in_4     = 'fbss_tp24_pval0.1_weekly_europe_annual_2021-01-04_2021-12-30.nc'
 figname_out       = 'fig_04.pdf'
 
 # read in data
 ds1        = xr.open_dataset(path_in + filename_in_1)
-ds2        = xr.open_dataset(path_in + filename_in_2)
-ds3        = xr.open_dataset(path_in + filename_in_3)
+ds2        = xr.open_dataset(path_in + filename_in_2)    
+ds3        = xr.open_dataset(path_in + filename_in_3)                                                                                           
 ds4        = xr.open_dataset(path_in + filename_in_4)
-ds5        = xr.open_dataset(path_in + filename_in_5)
-ds6        = xr.open_dataset(path_in + filename_in_6)
+
+# Remove box sizes where low and high-res data don't overlap on the same grid in timescale dimension data
+index         = np.where(~np.isnan(ds2['fbss'][:,4]))[0]
+ds2           = ds2.isel(box_size=index)
+ds4           = ds4.isel(box_size=index)
 
 # calculate significance
 sig1       = s2s.mask_significant_values_from_bootstrap(ds1['fbss_bootstrap'],0.05)
 sig2       = s2s.mask_significant_values_from_bootstrap(ds2['fbss_bootstrap'],0.05)
 sig3       = s2s.mask_significant_values_from_bootstrap(ds3['fbss_bootstrap'],0.05)
 sig4       = s2s.mask_significant_values_from_bootstrap(ds4['fbss_bootstrap'],0.05)
-sig5       = s2s.mask_significant_values_from_bootstrap(ds5['fbss_bootstrap'],0.05)
-sig6       = s2s.mask_significant_values_from_bootstrap(ds6['fbss_bootstrap'],0.05)
 
 # plot 
 fontsize   = 11
 clevs      = np.arange(0.0, 1.1, 0.1)
-clevs_anom = np.arange(-0.5, 0.55, 0.05)
-cmap       = mpl.cm.get_cmap("GnBu").copy()
-cmap_anom  = mpl.cm.get_cmap("RdBu_r").copy()
-figsize    = np.array([15,7.5])
-fig,ax     = plt.subplots(nrows=2,ncols=3,sharey='row',sharex='col',figsize=(figsize[0],figsize[1]))
+cmap1      = mpl.cm.get_cmap("PuBu").copy()
+cmap2      = mpl.cm.get_cmap("YlOrBr").copy()
+figsize    = np.array([12,8])
+fig,ax     = plt.subplots(nrows=2,ncols=2,sharey='row',sharex='col',figsize=(figsize[0],figsize[1]))
 ax         = ax.ravel()
 
-#cmap_anom = misc.create_custom_colormap_with_white_center(cmap_anom, clevs_anom)
+# A) precipitation 90th quantile
+setup_subplot(ax[0], ds1['time'], ds1['box_size'], ds1['fbss'], sig1, 'a) >90$^{th}$ quantile daily-mean precipitation', clevs, cmap1, fontsize)
 
+# B) weekly precipitation 
+setup_subplot(ax[1], ds2['time'], ds2['box_size'], ds2['fbss'], sig2, 'b) >90$^{th}$ quantile weekly-mean precipitation', clevs, cmap1, fontsize)
 
-# A) precipitation
-setup_subplot(ax[0], ds1['time'], ds1['box_size'], ds1['fbss'], sig1, 'a) >90$^{th}$ quantile precipitation', clevs, cmap, fontsize)
+# C) temperature 90th quantile
+setup_subplot(ax[2], ds3['time'], ds3['box_size'], ds3['fbss'], sig3, 'c) <10$^{th}$ quantile daily-mean precipitation', clevs, cmap2, fontsize)
 
-# B) temperature 
-setup_subplot(ax[1], ds2['time'], ds2['box_size'], ds2['fbss'], sig2, 'b) <10$^{th}$ quantile precipitation', clevs, cmap, fontsize)
-
-# C) precip minus temperature 
-setup_subplot(ax[2], ds3['time'], ds3['box_size'], ds3['fbss'], sig3, 'c) >90$^{th}$ minus <10$^{th}$ quantile precipitation', clevs_anom, cmap_anom, fontsize)
-
-# D) precip 
-setup_subplot(ax[3], ds4['time'], ds4['box_size'], ds4['fbss'], sig4, 'd) >90$^{th}$ quantile temperature', clevs, cmap, fontsize)
-
-# E) temperature 
-setup_subplot(ax[4], ds5['time'], ds5['box_size'], ds5['fbss'], sig5, 'e) <10$^{th}$ quantile temperature', clevs, cmap, fontsize)
-
-# F) temperature minus precipitation 
-setup_subplot(ax[5], ds6['time'],ds6['box_size'], ds6['fbss'], sig6, 'f) >90$^{th}$ minus <10$^{th}$ quantile temperature', clevs_anom, cmap_anom, fontsize)
+# D) weekly temperature
+setup_subplot(ax[3], ds4['time'], ds4['box_size'], ds4['fbss'], sig4, 'd) <10$^{th}$ quantile weekly-mean precipitation', clevs, cmap2, fontsize)
 
 # write2file
 plt.tight_layout()
